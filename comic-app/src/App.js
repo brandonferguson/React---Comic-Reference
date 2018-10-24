6859979c5ada375be95fa555385ee6871ac0d07f
Brandon Ferguson <syntaxc@gmail.com>
2018-10-23 20:43:11 -0400

    Added Characters Component and Next/Prev buttons

--- comic-app/src/App.js
+++ comic-app/src/App.js
@@ -1,29 +1,12 @@
 import React, { Component } from "react";
-
-let apiKey = "9f16f47974b9547b7c71317f3308c389645cd1c0";
+import Characters from "./Characters";
 
 class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      character: []
-    };
-  }
-
-  async componentDidMount() {
-    const character = await fetch(
-      "https://comicvine.gamespot.com/api/characters/?api_key=9f16f47974b9547b7c71317f3308c389645cd1c0&format=json/"
-    );
-    const json = await character.json();
-    this.setState({ character: json.results });
-  }
-
   render() {
     return (
       <div className="App">
         <div>
-          <input type="text" />
-          <p />
+          <Characters />
         </div>
       </div>
     );
---
+++ comic-app/src/Characters.js
@@ -0,0 +1,78 @@
+import React, { Component } from "react";
+import "./index.css";
+
+let apikey = "c91f99ccbccb1552c77fdefabe41a66e";
+
+class Characters extends Component {
+  constructor() {
+    super();
+    this.state = {
+      characters: [],
+      offsetCount: 0
+    };
+  }
+
+  async componentDidMount() {
+    const characters = await fetch(
+      `https://gateway.marvel.com/v1/public/characters?apikey=${apikey}&offset=0`
+    );
+    const json = await characters.json();
+    this.setState({ characters: json.data.results });
+  }
+
+  async componentWillUpdate() {
+    const characters = await fetch(
+      `https://gateway.marvel.com/v1/public/characters?apikey=${apikey}&offset=${
+        this.state.offsetCount
+      }`
+    );
+    const json = await characters.json();
+    this.setState({ characters: json.data.results });
+  }
+
+  decrementOffset = () => {
+    this.setState({ offsetCount: this.state.offsetCount - 20 });
+  };
+
+  incrementOffset = () => {
+    this.setState({ offsetCount: this.state.offsetCount + 20 });
+  };
+
+  render() {
+    const { characters } = this.state;
+
+    return (
+      <div className="row">
+        {characters.map(c => (
+          <div className="card col-sm-3">
+            {c.name}
+            <img
+              src={
+                c.thumbnail.path + "/standard_medium." + c.thumbnail.extension
+              }
+              className="thumbnail"
+            />
+          </div>
+        ))}
+        <div className="buttons">
+          <button
+            type="button"
+            className="prev btn btn-primary"
+            onClick={this.decrementOffset}
+          >
+            Prev
+          </button>
+          <button
+            type="button"
+            className="next btn btn-primary"
+            onClick={this.incrementOffset}
+          >
+            Next
+          </button>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Characters;
